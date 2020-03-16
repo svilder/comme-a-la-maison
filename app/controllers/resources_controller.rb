80b36d35aa2bffa0c3eefb16feae90a3f1d0a508
Sandra Vilder <sandra.vilder@gmail.com>
2020-03-16 18:16:32 +0100

    routes et controller CRUD actions + basic views

---
+++ app/controllers/resources_controller.rb
@@ -0,0 +1,39 @@
+class ResourcesController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :index ]
+
+  def index
+    @resrouces = Resource.where(state: "approved")
+  end
+
+  def new
+    @resource = Resource.new
+  end
+
+  def create
+    @resource = Resource.new(resource_params)
+    if @resource.save
+      redirect_to resources_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @resource = Resource.find(params[:id])
+  end
+
+  def update
+    @resource = Resource.find(params[:id])
+    if @resource.update(params[:resource])
+      redirect_to resources_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:description, :link, :public, :objectif, :activity_type)
+  end
+end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,10 +1,14 @@
 class Resource < ApplicationRecord
   belongs_to :user
+  PUBLIC = ["Enfants", "Adultes"]
+  OBJECTIF = ["Apprendre", "Se divertir"]
+  ACTIVITIES = ["regarder", "jouer", "écouter", "lire", "fabriquer"]
+  STATES = ["pending", "approved", "denied"]
 
   validates :description, :link, :public, :objectif, :activity_type, presence: true
-  validates :public, inclusion: { in: ["kids", "adults"] }
-  validates :objectif, inclusion: { in: ["learn", "fun"] }
-  validates :activity_type, inclusion: { in: ["watch", "play", "listen", "read", "make"] }
-  validates :state, inclusion: { in: ["pending", "approved", "denied"] }
+  validates :public, inclusion: { in: PUBLIC }
+  validates :objectif, inclusion: { in: OBJECTIF }
+  validates :activity_type, inclusion: { in: ACTIVITIES }
+  validates :state, inclusion: { in: STATES }
   validates :link, format: { with: /\b((http|https):\/\/)[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/?))/i, message: "please enter a correct URL" }
 end
