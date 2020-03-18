412cc121e06af9e887b0c5d3bc7f46f86d531ef0
Sandra Vilder <sandra.vilder@gmail.com>
2020-03-18 16:02:50 +0100

    migration simplify

--- db/migrate/20200316153436_create_resources.rb
+++ db/migrate/20200316153436_create_resources.rb
@@ -3,9 +3,6 @@ class CreateResources < ActiveRecord::Migration[6.0]
     create_table :resources do |t|
       t.string :description
       t.string :link
-      t.string :public
-      t.string :objectif
-      t.string :activity_type
       t.string :state, default: "pending"
       t.references :user, null: false, foreign_key: true
 
