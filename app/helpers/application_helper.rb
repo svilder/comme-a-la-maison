8446368ceee3276e48c7f2fee111038d57e45d34
Sandra Vilder <sandra.vilder@gmail.com>
2020-03-31 17:32:44 +0200

    pas de link hover et puis voila

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,4 +46,13 @@ module ApplicationHelper
       "box"
     end
   end
+
+  def truncate(string, max_length)
+    str = string.to_s
+    if str.length > max_length
+      str[0...max_length] + "..."
+    else
+      str
+    end
+  end
 end
