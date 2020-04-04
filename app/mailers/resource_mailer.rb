fccc4a493f2962dcbf63127759d665652662ee99
Sandra Vilder <sandra.vilder@gmail.com>
2020-04-04 17:33:49 +0200

    fixed cards and link on hover

--- app/mailers/resource_mailer.rb
+++ app/mailers/resource_mailer.rb
@@ -1,6 +1,8 @@
 class ResourceMailer < ApplicationMailer
   def thanks
     @user = params[:user]
+    # @email = AdminUser.find_by(send_emails: true).email
+
     mail(to: @user.email, subject: 'Merci pour votre participation')
   end
 
@@ -8,4 +10,5 @@ class ResourceMailer < ApplicationMailer
     @user = params[:user]
     mail(to: @user.email, subject: 'Votre ressource a été publiée')
   end
+
 end
